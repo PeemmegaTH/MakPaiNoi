bf5360a21c6d4873ba761a0cd46f1ea7191ee17a
PeemmegaTH <theeridth.yodk@kmutt.ac.th>
2023-04-05 21:58:29 +0700

    MakPaiNoi

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
 ﻿class ArrayMoment {
 
-
-    // static int check(int a,int b){
-    //     return;
-    // }
-
     static void Swap(ref int a , ref int b){
         int tmp = b;
         b = a;
         a = tmp;
     }
+
     static void Main(string[] args){
         
+        // string[,] sussy = new string[3,3];
+
+        
+        // for (int row = 0;row < 3; row++){
+        //     for (int colume = 0;colume < 3; colume++){
+        //         Console.WriteLine(row + " " + colume );
+        //         sussy[row,colume] = "#";
+        //     }
+        //     Console.WriteLine();
+        // }
+
+        // for (int row = 0;row < 3; row++){
+        //     for (int colume = 0;colume < 3; colume++){
+        //         Console.Write(sussy[row,colume] + " ");
+        //     }
+        //     Console.WriteLine();
+        // }
+
         Console.Write("Input Max Array: ");
         int Max = int.Parse(Console.ReadLine());
         int[] table = new int[Max];
@@ -32,8 +46,6 @@
         for (int i = 0;i < Max;i++){
             Console.Write((table[i]) + " ");
         }
-
-        //Console.Write(" is ever number.");
 
     }
 }
